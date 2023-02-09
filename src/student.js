d3c9a41b35b6578f5166b7bca0b01a45a81491f9
leejisoo <leejisoo@atnsgroup.com>
2023-02-10 08:46:12 +0900

    React-reduce2

---
+++ src/student.js
@@ -0,0 +1,19 @@
+const Student = ({name, dispatch, id, isHere}) => {
+    return(
+        <div>
+            <span
+                style={{
+                    textDecoration : isHere ? 'line-through' : 'none',
+                    color : isHere ? 'gray' : 'black',
+                }}
+                onClick={()=>{
+                    dispatch({type:'mark-student', payload:{id}})
+                }}>{name}</span>
+            <button onClick={()=>{
+                dispatch({type:'delete-student',payload:{id}})
+            }}>삭제</button>
+        </div>
+    );
+};
+
+export default Student;
